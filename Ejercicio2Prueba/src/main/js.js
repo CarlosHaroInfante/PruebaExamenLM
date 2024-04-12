6e069d0c47aa2c3a2b99875b033f54f13f2cdb19
CarlosHaroInfante <145343573+CarlosHaroInfante@users.noreply.github.com>
2024-04-12 14:39:50 +0200

    Add files via upload

---
+++ Ejercicio2Prueba/src/main/js.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+
+ function admin(){
+	 
+	 var contrasenia = "salida";
+	 
+	 var numIntentosFallidos = 0;
+	 
+	 var numIntentosGanados = 0;
+	 
+	 var porcentajeAcierto = numIntentosFallidos / numIntentosGanados * 100
+	 
+	 
+ }
+ 
+ function clientes(){
+	 
+	 var intentos = 3;
+	 
+	 for (i = 0; i = intentos; i++){
+		 
+		 if(i < 3){
+			 alert("Siguiente oportunidad");
+			 
+		 }
+		 
+	 }
+ }
---
+++ PruebaExamen1/src/main/js.js
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+var num = "0";
+var texto = "";
+
+ function numeros(eleccionUsu1 ){
+ 
+ var num0 = 0;
+ var num1 = 1;
+ var num2 = 2;
+ var num3 = 3;
+ var num4 = 4;
+ var num5 = 5;
+ var num6 = 6;
+ var num7 = 7;
+ var num8 = 8;
+ var num9 = 9;
+ 
+ var eleccionUsu1;
+ 
+
+ 
+ if(eleccionUsu1 == 0){
+	num0
+ }
+ if(eleccionUsu1 == 1){
+	 num1
+ }
+  if(eleccionUsu1 == 2){
+	 num2
+ }
+  if(eleccionUsu1 == 3){
+	 num3
+ }
+  if(eleccionUsu1 == 4){
+	 num4
+ }
+  if(eleccionUsu1 == 5){
+	 num5
+ }
+  if(eleccionUsu1 == 6){
+	 num6
+ }
+  if(eleccionUsu1 == 7){
+	 num7
+ }
+  if(eleccionUsu1 == 8){
+	 num8
+ }
+  if(eleccionUsu1 == 9){
+	 num9
+ }
+ 
+ num = 10 + eleccionUsu1;
+ texto += eleccionUsu1;
+ document.getElementById("num").innerHTML = texto;
+
+
+ 
+ }
+ 
+ function suma(texto){
+	 
+	 var simbolo = "+";
+	 var texto2 = texto;
+	
+	 
+	 
+	 
+ }
+ 
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
